b732b46f1408c9026864960aba4e4f7e790b5c07
adAstra97 <131522998+adAstra97@users.noreply.github.com>
2024-11-04 23:49:59 +0300

    fix: correct chart & some styles

--- src/app/coins/components/coin-detail/coin-detail.component.ts
+++ src/app/coins/components/coin-detail/coin-detail.component.ts
@@ -7,7 +7,10 @@ import {
   OnDestroy,
 } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ICoin } from '../../../shared/models/coin-response.model';
+import {
+  ICoin,
+  IHistoryItem,
+} from '../../../shared/models/coin-response.model';
 import { CoinService } from '../../services/coin.service';
 import { Chart, registerables } from 'chart.js';
 import { PortfolioService } from '../../../core/services/portfolio.service';
@@ -55,7 +58,7 @@ export class CoinDetailComponent implements OnInit, AfterViewInit, OnDestroy {
   }
 
   ngAfterViewInit(): void {
-    this.loadChart('d1');
+    this.loadChart('24h');
   }
 
   ngOnDestroy(): void {
@@ -110,38 +113,78 @@ export class CoinDetailComponent implements OnInit, AfterViewInit, OnDestroy {
   private loadChart(interval: string): void {
     const id = this.route.snapshot.paramMap.get('id');
 
-    if (id) {
-      this.subscriptions.push(
-        this.coinService.getCoinHistory(id, interval).subscribe({
-          next: data => {
-            const prices = data.map(item => item.priceUsd);
-            const labels = data.map(item =>
-              new Date(item.time).toLocaleTimeString()
-            );
+    if (!id) {
+      console.error('Coin ID is not available');
+      return;
+    }
 
-            if (this.chart) {
-              this.chart.destroy();
-            }
+    const requestInterval = this.getRequestInterval(interval);
+    this.subscriptions.push(
+      this.coinService.getCoinHistory(id, requestInterval).subscribe({
+        next: data => this.handleChartData(data, interval),
+        error: () => console.error('Failed to load coin history'),
+      })
+    );
+  }
 
-            if (this.coinChartCanvas) {
-              this.chart = new Chart(this.coinChartCanvas.nativeElement, {
-                type: 'line',
-                data: {
-                  labels: labels,
-                  datasets: [
-                    {
-                      label: 'Price (USD)',
-                      data: prices,
-                      borderColor: 'rgba(75, 192, 192, 1)',
-                      fill: true,
-                    },
-                  ],
-                },
-              });
-            }
-          },
-        })
-      );
+  private getRequestInterval(interval: string): string {
+    return interval === '24h' || interval === '12h' ? 'h1' : 'm1';
+  }
+
+  private handleChartData(data: IHistoryItem[], interval: string): void {
+    const filteredData = this.filterDataByTime(data, interval);
+    const prices = filteredData.map(item => item.priceUsd);
+    const labels = filteredData.map(item =>
+      new Date(item.time).toLocaleTimeString()
+    );
+
+    this.updateChart(prices, labels);
+  }
+
+  private filterDataByTime(
+    data: IHistoryItem[],
+    interval: string
+  ): IHistoryItem[] {
+    const currentTime = new Date().getTime();
+    const timeThreshold = this.getTimeThreshold(currentTime, interval);
+
+    return data.filter(item => item.time >= timeThreshold);
+  }
+
+  private getTimeThreshold(currentTime: number, interval: string): number {
+    switch (interval) {
+      case '24h':
+        return currentTime - 24 * 60 * 60 * 1000;
+      case '12h':
+        return currentTime - 12 * 60 * 60 * 1000;
+      case '1h':
+        return currentTime - 1 * 60 * 60 * 1000;
+      default:
+        console.error('Invalid interval specified');
+        return currentTime;
+    }
+  }
+
+  private updateChart(prices: string[], labels: string[]): void {
+    if (this.chart) {
+      this.chart.destroy();
+    }
+
+    if (this.coinChartCanvas) {
+      this.chart = new Chart(this.coinChartCanvas.nativeElement, {
+        type: 'line',
+        data: {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Price (USD)',
+              data: prices,
+              borderColor: 'rgba(75, 192, 192, 1)',
+              fill: true,
+            },
+          ],
+        },
+      });
     }
   }
 }
